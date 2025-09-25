84f81653ef561567be7f0e7ffa8b62b4ede8575e
jayraj-parki <jayrajfd@gmail.com>
2025-09-26 01:21:53 +0530

    temp

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,13 +1,7 @@
-import Registers from "@/components/register/Register";
+import Register from "@/components/Register/Register";
 
-export default function RegisterPage() {
-    
 
-    return (
-        <>
-            <main className="container rows mx-auto col-12 col-lg-10 mt-lg-4  mt-3">
-                <Registers />
-            </main>
-        </>
-    );
+
+export default function Page() {
+    return <div><Register/></div>;
 }
